d3096f7ca757692897a0134e84324f048471a827
MarcoVeneri <30596631+MarcoVeneri@users.noreply.github.com>
2026-09-21 19:40:36 +0200

    Aggiorna cache Alice Job Radar v6.9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="alice-job-radar-v6-8";
+const CACHE="alice-job-radar-v6-9";
 const SHELL=["./","./index.html","./config.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
